6a9ef5df085b30acbd0ddebd4051d4ac4d2a9125
Benjamin Walkenhorst <krylon@posteo.de>
2024-08-09 18:56:38 +0200

    Add sub-package extractor

---
+++ extractor/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2024-08-09 18:45:51 krylon>
+#
+# /data/code/python/silo/extractor/__init__.py
+# created on 09. 08. 2024
+# (c) 2024 Benjamin Walkenhorst
+#
+# This file is part of the Vox audiobook reader. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+silo.extractor.__init__
+
+(c) 2024 Benjamin Walkenhorst
+"""
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
