540d2c8e17cc5f43ac7359f8c105c146852b17f2
Benjamin Walkenhorst <krylon@posteo.de>
2024-08-09 18:38:35 +0200

    Add module common

---
+++ __init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2024-08-09 18:36:37 krylon>
+#
+# /data/code/python/silo/__init__.py
+# created on 09. 08. 2024
+# (c) 2024 Benjamin Walkenhorst
+#
+# This file is part of the Vox audiobook reader. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+silo.__init__
+
+(c) 2024 Benjamin Walkenhorst
+"""
+
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
